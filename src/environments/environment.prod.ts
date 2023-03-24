1bca0221cf1ae0089a89c74d1ed78d79b4c26c18
Yuri Vitor <yuri.lins25@gmail.com>
2023-03-24 14:36:32 -0300

    online api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl:
-    'http://localhost:8080/https://query2.finance.yahoo.com/v8/finance/chart/'
+    'https://yahoo-finance-api.herokuapp.com/api/https://query2.finance.yahoo.com/v8/finance/chart/'
 };
